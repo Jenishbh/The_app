d3827ea26fab92d9fd468a1f6be12aba4e936122
Jenishbh <Jenishpatel1999@gmail.com>
2022-06-02 08:27:11 -0700

    Hello

--- The_restaurant_app/App.js
+++ The_restaurant_app/App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView ,Alert, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <SafeAreaView style={styles.container}>
+      <Text >Hello Jenish</Text>
+      <Button color='orange'
+      title='Click Me'
+      onPress={()=>Alert.alert("Button press", "You press button", [ { text: "Yes"}, {text : "No"},]) }
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
