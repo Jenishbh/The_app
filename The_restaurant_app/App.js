cfaa256dc4cd51cb3ee02a9e8c3924aaa18be98d
Jenishbh <Jenishpatel1999@gmail.com>
2022-06-03 18:28:05 -0700

    Login background

--- The_restaurant_app/App.js
+++ The_restaurant_app/App.js
@@ -10,70 +10,9 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+import Login from "./app/screens/Login";
 
 export default function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('');
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello Jenish</Text>
-      <StatusBar style="auto" />
-      <SafeAreaView style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </SafeAreaView>
- 
-      <SafeAreaView style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </SafeAreaView>
- 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
- 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-}
+  return< Login />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
- 
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
- 
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
-  },
-});
+}
---
+++ The_restaurant_app/app/screens/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function Login(props) {
+    return (
+        <SafeAreaView style={styles.background}>
+            
+
+        </SafeAreaView>
+    );
+}
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor: 'gray',
+        flex: 1
+    }
+})
+export default Login;
