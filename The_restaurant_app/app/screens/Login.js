6c3f3bac9ed3266e3440cf1c5c3840d59e059afe
Jenishbh <Jenishpatel1999@gmail.com>
2022-06-04 09:11:26 -0700

    main

--- The_restaurant_app/app/screens/Login.js
+++ The_restaurant_app/app/screens/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {View, SafeAreaView, StyleSheet, Text, Image, ImageBackground, TextInput} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 function Login(props) {
     return (
         <SafeAreaView style={styles.background}>
+            <Image style={styles.logo} source={require('../assets/Logo1.png')} />
+            <TextInput style={styles.username}> Username</TextInput>
+            <TextInput style={styles.username}> Password</TextInput>
             
 
         </SafeAreaView>
@@ -11,8 +16,25 @@ function Login(props) {
 }
 const styles = StyleSheet.create({
     background:{
-        backgroundColor: 'gray',
-        flex: 1
+        backgroundColor: '#191E25',
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    logo:{
+        height: 107,
+        width: 300,        
+        position: 'absolute',
+        top: 180    
+    },
+    username:{
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 10,
+        padding: 8,
+        margin: 10,
+        width: 300,
+        top: -330,
     }
 })
 export default Login;
