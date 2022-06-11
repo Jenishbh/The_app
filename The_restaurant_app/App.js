1aef39ff2f8d17871f9361d21ddb344f88b3be92
sharoncao0920 <ycao25sharon@gmail.com>
2022-06-11 16:44:48 -0700

    test

--- The_restaurant_app/App.js
+++ The_restaurant_app/App.js
@@ -11,19 +11,35 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Login from "./app/screens/Login";
-import './node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import SignUp from "./app/screens/Registration";
+//import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import './App.css';
+//import SignUp from "./app/screens/Registration";
+import Forget_pass from "./app/screens/forget_pass";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  //return< Login />
-  //return "Hello"
-  return <SignUp />
-  return (
-    <div className="App">
-      <h3>Build Sign Up & Login UI Template in React</h3>
-    </div>
-  );
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.screens>
+                
+            </Stack.screens>
+
+        </Stack.Navigator>
+
+
+    </NavigationContainer>
+    return< Login />
+    //return < Forget_pass />
+    //return "Hello"
+    //return <SignUp />
+    //return (
+    //  <div className="App">
+    //    <h3>Build Sign Up & Login UI Template in React</h3>
+    //  </div>
+    //);
 }
 
 export default App;
--- The_restaurant_app/app/screens/Login.js
+++ The_restaurant_app/app/screens/Login.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {View, SafeAreaView, StyleSheet, Text, Image, ImageBackground, TextInput} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import {View, SafeAreaView, StyleSheet,Button, Image, ImageBackground,Text, TextInput,TouchableOpacity} from 'react-native';
+
+
+
 
 
 function Login(props) {
-    //<TextInput style={styles.username}> Username</TextInput>
-    //<TextInput style={styles.username}> Password</TextInput>
+    
     return (
         <SafeAreaView style={styles.background}>
             <Image style={styles.logo} source={require('../assets/Logo1.png')} />
+            <TextInput style={styles.username}> Username</TextInput>
+            <TextInput style={styles.username}> Password</TextInput>
+            <TouchableOpacity style={styles.signin} onPress={console.log('Forgot Press')}>
+                <Text > Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity  style={styles.signin}>
+                
+                <Button title='Signin' color='#F8B864' onPress={console.log('Sign In')} />
+            </TouchableOpacity>
+            <Text style={styles.line} >________ OR _______</Text>
+            <TouchableOpacity  style={styles.Signup}>
+                
+                <Button title='SignUp' color='#F8B864' onPress={console.log('Sign Up')} />
+            </TouchableOpacity>
+        
+            
+
 
         
         </SafeAreaView>
@@ -34,7 +52,29 @@ const styles = StyleSheet.create({
         padding: 8,
         margin: 10,
         width: 300,
-        top: -330,
+        top: -250,
+        color: 'white'
+        
+    },
+    signin:{  
+        top: -240,
+        
+        
+    },
+    line:{
+        top: -230,
+        color: 'white',
+    },
+    Signup:{
+        borderWidth: 1,
+        borderColor: 'orange',
+        borderRadius: 10,
+        padding: 8,
+        
+        width: 300,
+        top: -180,
+        color: 'white'
     }
 })
+
 export default Login;
