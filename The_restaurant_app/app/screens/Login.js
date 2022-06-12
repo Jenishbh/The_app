555fa00cdc118ffbf70ba04a0949859290775930
Jenishbh <Jenishpatel1999@gmail.com>
2022-06-11 17:32:14 -0700

    New Login

--- The_restaurant_app/app/screens/Login.js
+++ The_restaurant_app/app/screens/Login.js
@@ -1,40 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import {View, SafeAreaView, StyleSheet,Button, Image, ImageBackground,Text, TextInput,TouchableOpacity} from 'react-native';
-
-
-
-
-
-function Login(props) {
-    
-    return (
-        <SafeAreaView style={styles.background}>
-            <Image style={styles.logo} source={require('../assets/Logo1.png')} />
-            <TextInput style={styles.username}> Username</TextInput>
-            <TextInput style={styles.username}> Password</TextInput>
-            <TouchableOpacity style={styles.signin} onPress={console.log('Forgot Press')}>
-                <Text > Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity  style={styles.signin}>
-                
-                <Button title='Signin' color='#F8B864' onPress={console.log('Sign In')} />
-            </TouchableOpacity>
-            <Text style={styles.line} >________ OR _______</Text>
-            <TouchableOpacity  style={styles.Signup}>
-                
-                <Button title='SignUp' color='#F8B864' onPress={console.log('Sign Up')} />
-            </TouchableOpacity>
-        
-            
-
-=======
-
 import React, { useState,useEffect} from 'react';
 import { useNavigation } from '@react-navigation/native';
-import {View, SafeAreaView, StyleSheet,Button, Image, ImageBackground,Text, TextInput,TouchableOpacity, Pressable} from 'react-native';
-import Forget_pass from './Forget_pass';
-import SignUp from './Registration';
+import {View, SafeAreaView, StyleSheet,Button, Image, ImageBackground,Text, TextInput,TouchableOpacity} from 'react-native';
 
 import { auth } from '../database/firebase';
 
@@ -42,15 +8,12 @@ import {getAuth, OnAuthStateChanged, signInWithEmailAndPassword, createUserWithE
 
 
 
-
-function Login() {
-    
-
-   const[email, setEmail]= useState('')    // set Email and password varible for cath user input
+function Login(props) {
+    const[email, setEmail]= useState('')    // set Email and password varible for cath user input
    const[password, setPassword] = useState('')
    const navigation = useNavigation()      // set navigation to change the screen after user login
 
-    
+
    useEffect(() => { 
     const auth=getAuth();
                         //function for user to move on to home screen after  login 
@@ -61,7 +24,7 @@ function Login() {
     })
 
     return unsubscribe
-    
+
    }, [])
 
    const handleSignup = () =>{  //handle sigup
@@ -79,7 +42,7 @@ function Login() {
 
    const handleLogin = () =>{    
     const auth = getAuth();  //Handel Login by firebase
-    
+
         signInWithEmailAndPassword(auth, email, password)
         .then(userCredentials => {
             const user = userCredentials.user;
@@ -89,10 +52,8 @@ function Login() {
    }
 
 
-
-
-    return (   
-        <SafeAreaView style={styles.background}>    
+    return (
+        <SafeAreaView style={styles.background}>
             <Image style={styles.logo} source={require('../assets/Logo1.png')} />
             <TextInput
              placeholder='Email'
@@ -100,7 +61,34 @@ function Login() {
              onChangeText={text => setEmail(text)}
              style={styles.username}
              />
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
+
+            <TextInput 
+            placeholder='Password'
+            value={password}
+            onChangeText={text => setPassword(text)}
+            style={styles.username}
+             secureTextEntry
+             />
+            <TouchableOpacity  style={styles.line}>  
+            <Button title='Forgot Password?'  onPress={() => navigation.navigate(Forget_pass)} />
+            </TouchableOpacity>
+
+            <Pressable  style={styles.signin}>
+
+                <Button title='Login' color='#F8B864' onPress={handleLogin} />
+            </Pressable>
+
+
+            <Text style={styles.line} >________ OR _______</Text>
+
+
+            <TouchableOpacity  style={styles.Signup}>
+                
+                <Button title='SignUp' color='#F8B864' onPress={handleSignup} />
+            </TouchableOpacity>
+        
+            
+
 
             <TextInput 
             placeholder='Password'
@@ -146,29 +134,17 @@ const styles = StyleSheet.create({
         padding: 8,
         margin: 10,
         width: 300,
-<<<<<<< HEAD
         top: -250,
-=======
-        top: -190,
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
         color: 'white'
         
     },
     signin:{  
-<<<<<<< HEAD
         top: -240,
-=======
-        top: -170,
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
         
         
     },
     line:{
-<<<<<<< HEAD
         top: -230,
-=======
-        top: -170,
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
         color: 'white',
     },
     Signup:{
@@ -178,19 +154,12 @@ const styles = StyleSheet.create({
         padding: 8,
         
         width: 300,
-<<<<<<< HEAD
-        top: -180,
-=======
         top: -110,
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
         color: 'white'
     }
 })
 
-<<<<<<< HEAD
 export default Login;
-=======
 
 
-export default Login;
->>>>>>> 6d1ef3769f4edfe814e7608dd977f8715833248d
+
