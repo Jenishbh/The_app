ef6e4a2b1dd1dc3f77f799843be2a2d316ef0b84
sharoncao0920 <ycao25sharon@gmail.com>
2022-06-22 17:30:59 -0700

    manager_update

--- The_restaurant_app/app/screens/Manager/Manager_home.js
+++ The_restaurant_app/app/screens/Manager/Manager_home.js
@@ -1,148 +1,158 @@
-import { View, Text } from 'react-native'
-import React, {useState} from 'react'
-import { SafeAreaView } from 'react-navigation';
-import CircularProgress from 'react-native-circular-progress-indicator';
-
+import React,{useState, setValue} from 'react';
+import {View, Switch, SafeAreaView, StyleSheet,Button, Image, ImageBackground,Text, TextInput,TouchableOpacity} from 'react-native';
 
 
 export default function Manager_home(props) {
-    const buttonClickedHandler = () => {
-        console.log('You have press the switch!');
-    };
+  const buttonClickedHandler = () => {
+      console.log('You have press the switch!');
+  };
 
-    const [value, setValue] = useState("ON");
+  const [value, setValue] = useState("ON");
 
-    const switchPressed = () => {
-        console.log('Switch pressed');
-        setValue("OFF");
-    };
-    
-    const CButton = ({ text }) => {
-        return (
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.text}>{text}</Text>
-          </TouchableOpacity>
-        );
-    };
-
-    return (
-        <SafeAreaView style={styles.background}>
-            <CircularProgress
-                radius={90}
-                value={80}
-                textColor='#222'
-                fontSize={20}
-                valueSuffix={'%'}
-                inActiveStrokeColor={'#2ecc71'}
-                inActiveStrokeOpacity={0.2}/>
-          <Image style={styles.logo} source={require('../pics/Logo1.png')} />
-          <Text style={styles.check_in}> Checked-In </Text>
-            <TouchableOpacity
-                onPress={buttonClickedHandler}
-                style={styles.roundButton1}>
-                <Text style={styles.number}>08</Text>  
-            </TouchableOpacity>
-            
-            <View style={styles.container1}>
-                <View style={styles.parent1}>
-                    <CButton text={"MENU"} />
-                    <CButton text={"REPORTS"} />
-                </View>
-                <TouchableOpacity onPress={buttonClickedHandler} style={styles.roundButton2}>
-                    <Text style={styles.switch} onPress={switchPressed}> {value}</Text>            
-                </TouchableOpacity>
-            </View>
-
-        </SafeAreaView>
-    
+  const switchPressed = () => {
+      console.log('Switch pressed');
+      setValue("OFF");
+  };
+  
+  const CButton = ({ text }) => {
+      return (
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
       );
+  };
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+      <SafeAreaView style={styles.background}>
+        <Image style={styles.logo} source={require('../pics/table-management.png')} />
+        <Text style={styles.check_in}> Checked-In </Text>
+          <TouchableOpacity
+              onPress={buttonClickedHandler}
+              style={styles.roundButton1}>
+
+              <Text style={styles.number}>08</Text>  
+          </TouchableOpacity>
+          
+          <View style={styles.switchBotton}>
+          <Text style={styles.onoff}>Slide to Turn On/Off Pre-check-In</Text>  
+          <Switch
+              trackColor={{ false: "#767577", true: "#767577" }}
+              thumbColor={isEnabled ? "#0BF10B" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              activeText={'On'}
+              inActiveText={'Off'}
+              value={isEnabled}
+            />
+          </View>
+
+          <View style={styles.container1}>
+              <View style={styles.parent1}>
+                  <CButton text={"MENU"} />
+                  <CButton text={"REPORTS"} />
+            </View>
+              
+          </View>
+
+      </SafeAreaView>
+  
+    );
 }
-    const styles = StyleSheet.create({
-    background:{
-      backgroundColor: '#548B4F',
-      flex: 1,
-      alignItems: 'center'
-    },
-  
-    logo:{
-      height: 120,
-      width: 340,        
-      top: 140
-    },
-  
-    check_in:{
-      color: 'white',
-      fontFamily:'sans-serif',
-      top: 260,
-      fontSize: 32,
-      alignContent: 'center',
+  const styles = StyleSheet.create({
+  background:{
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  logo:{
+    height: 220,
+    width: 400,        
+    top: 80
+  },
+
+  check_in:{
+    color: 'orange',
+    fontFamily:'sans-serif',
+    top: 150,
+    fontSize: 32,
+    alignContent: 'center',
+    alignItems: 'center',
+    fontWeight:'bold',
+    
+  },
+
+  roundButton1:{
+      width: 150,
+      height: 150,
+      top: 180,
+      justifyContent: 'center',
       alignItems: 'center',
-      fontWeight:'bold',
+      padding: 10,
+      borderRadius: 100,
+      backgroundColor: 'orange',
+  },
+
+  number:{
+    margin: 1,
+    width: 300,
+    height: 60,
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 34
+  },
+
+  switchBotton: {
+      flex: 2,
+      alignItems: "center",
+      justifyContent: "center",
+      top:150
       
-    },
+  },
+
+  onoff:{
+    margin: 1,
+    width: 300,
+    height: 60,
+    color: 'grey',
+    textAlign: 'center',
+    fontSize: 12
+  },
   
-    roundButton1:{
-        width: 150,
-        height: 150,
-        top: 300,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-        borderRadius: 100,
-        backgroundColor: 'white',
-    },
-  
-    number:{
+  switch:{  
       margin: 1,
       width: 300,
       height: 60,
-      color: 'black',
+      color: 'white',
       textAlign: 'center',
       fontSize: 34
-    },
+  },
   
-    roundButton2:{
-        width:100,
-        height: 100,
-        borderRadius: 100,
-        backgroundColor: '#548B4F',
-        borderWidth: 1,
-        borderColor:'white',
-        alignSelf: 'center',
-        alignItems:'center',
-        top:-120
-    },
+  button: {
+      backgroundColor: "orange",
+      padding: 18,
+      width: "46%",
+      height: 60,
+      borderRadius:10
+  },
+  text: {
+      fontSize: 18,
+      color: "white",
+      textAlign: "center",
+      fontWeight:'bold'
+  },
 
-    switch:{  
-        margin: 1,
-        width: 300,
-        height: 60,
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 34
+  container1: {
+      flex: 1,
+  },
+  
+  parent1: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      top: 80
     },
-    
-    button: {
-        backgroundColor: "white",
-        padding: 18,
-        width: "46%",
-        height: 60,
-    },
-    text: {
-        fontSize: 18,
-        color: "#548B4F",
-        textAlign: "center",
-        fontWeight:'bold'
-    },
-
-    container1: {
-        flex: 1,
-    },
-
-    parent1: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        top: 450
-      },
-  });
+});
